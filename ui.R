4569ac3d558655a01606ee0d169ac8780eb1b901
unknown <nbyers@2UA43819Z9.state.in.us>
2015-01-13 13:47:49 -0500

    changed to basicPage

--- ui.R
+++ ui.R
@@ -7,32 +7,27 @@
 
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(basicPage(
   
-  sidebarPanel(
-    tags$head(
-      tags$style("body {background-color: #f6f6f6; }")
-    ),
-    
-    selectInput("pollutant",
-                "Pollutant:",
-                c("Ozone" = "o3",
-                "PM2.5" = "pm25"))
+  
+  tags$head(
+    tags$style("body {background-color: #f6f6f6; }")
   ),
+  
+  selectInput("pollutant",
+              "Pollutant:",
+              c("Ozone" = "o3",
+                "PM2.5" = "pm25")),
   
   conditionalPanel(
     condition = "input.pollutant == 'o3'",
     radioButtons("threshold", "Threshold:",
-                       c("75 ppb" = "75",
-                         "70 ppb" = "70",
-                         "65 ppb" = "65"),
-                       selected = "75")
+                 c("75 ppb" = "75",
+                   "70 ppb" = "70",
+                   "65 ppb" = "65"),
+                 selected = "75")
   ),
   
-  mainPanel(
-    tags$head(
-      tags$style("body {background-color: #f6f6f6; }")
-    ),
+  
     uiOutput("probMap")
-  )
 ))
